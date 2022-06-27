154da030446c77af2db208695fe0b185bd1b7d23
tchiunam <tchiunam@users.noreply.github.com>
2022-06-27 23:38:12 +0800

    Add helper function for generating timestamp string

--- lib/axolpy/util/helper/__init__.py
+++ lib/axolpy/util/helper/__init__.py
@@ -1,6 +1,7 @@
 import random
+import time
 
-__all__ = ["get_timestamp_string"]
+__all__ = ["get_random_bits", "get_timestamp_string"]
 
 
 def get_random_bits(bits: int = 128) -> str:
@@ -21,3 +22,23 @@ def get_random_bits(bits: int = 128) -> str:
         return get_random_bits(bits)
     else:
         return s
+
+
+def get_timestamp_string(date_separator: str = "", time_separator: str = "", component_separator: str = "") -> str:
+    """
+    Get timestamp in a compact string format.
+
+    :param date_separator: Separator within date components.
+    :type date_separator: str
+    :param time_separator: Separator within time components.
+    :type time_separator: str
+    :param component_separator: Separator between date / time components.
+    :type component_separator: str
+
+    :return: Current timestamp string in "%Y%m%d%H%M%S" format.
+    :rtype: str
+    """
+
+    return time.strftime("%Y{0}%m{0}%d{1}%H{2}%M{2}%S".format(date_separator,
+                                                              component_separator,
+                                                              time_separator), time.localtime())
