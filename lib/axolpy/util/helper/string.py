669007078f187a51abd84e3d2fd986d5479dbb14
tchiunam <tchiunam@users.noreply.github.com>
2022-06-27 23:53:33 +0800

    Add helper function for coverting camlcase string to underscroll separated

--- lib/axolpy/util/helper/string.py
+++ lib/axolpy/util/helper/string.py
@@ -1,6 +1,21 @@
 import hashlib
+import re
 
-__all__ = ["get_string_hex"]
+__all__ = ["camelcase_to_underscore", "get_string_hex"]
+
+
+def camelcase_to_underscrollsep(string: str) -> str:
+    """
+    Convert a *string* from camelcase to underscroll sepearated.
+
+    :param string: This is a string to be converted.
+    :type string: str
+
+    :return: Converted string.
+    :rtype: str
+    """
+
+    return re.sub(r"(^|[a-z])([A-Z])", lambda m: "_".join([i.lower() for i in m.groups() if i]), string)
 
 
 def get_string_hex(input_string: str) -> str:
