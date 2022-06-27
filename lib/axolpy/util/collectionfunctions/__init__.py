92736b0144494692b91865d7d2a7f1378573b228
tchiunam <tchiunam@users.noreply.github.com>
2022-06-28 00:11:43 +0800

    Add collection function for expanding iterable

---
+++ lib/axolpy/util/collectionfunctions/__init__.py
@@ -0,0 +1,26 @@
+from typing import Any, Callable, Generator, Iterable
+
+
+def expand_iter(elements: Iterable,
+                from_elements: Iterable,
+                expand_func: Callable[[Any], Iterable]) -> Generator[Any, None, None]:
+    """
+    Expand *from_element* in *elements*.
+
+    :param elements: Original set of elements.
+    :type elements: Iterable
+    :param from_elements: Elements that need expansion.
+    :type from_elements: Iterable
+    :param expand_func: Function to expand
+    :type expand_func: Callable[[Any], Iterable])
+
+    :return: Expanded elements.
+    :rtype: Generator[Any, None, None]
+    """
+
+    for i in elements:
+        if i in from_elements:
+            for j in expand_func(i):
+                yield j
+        else:
+            yield i
