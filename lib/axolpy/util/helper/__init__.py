5136bc4fdb067dcde97e1bfda944b3998150845a
tchiunam <tchiunam@users.noreply.github.com>
2022-06-27 23:58:10 +0800

    Add helper function for checking if a file is a text file

--- lib/axolpy/util/helper/__init__.py
+++ lib/axolpy/util/helper/__init__.py
@@ -1,7 +1,47 @@
+import io
 import random
 import time
 
-__all__ = ["get_random_bits", "get_timestamp_string"]
+__all__ = ["is_text_file", "get_random_bits", "get_timestamp_string"]
+
+
+def is_text_file(cls, file_: io.FileIO, blocksize: int = 512) -> bool:
+    """
+    Uses heuristics to guess whether the given file is text or binary,
+    by reading a single block of bytes from the file.
+    If more than 30% of the chars in the block are non-text, or there
+    are NUL ('\x00') bytes in the block, assume this is a binary file.
+
+    :param file_: This is a file to check.
+    :type file_: io.FileIO
+    :param blocksize: Block size to read.
+    :type blocksize: int
+
+    :return: True if *file_* is a text file.
+    :rtype: bool
+    """
+
+    # A function that takes an integer in the 8-bit range and returns
+    # a single-character byte object in py3 / a single-character string
+    # in py2.
+    int2byte = (lambda x: bytes((x,))) if cls.is_py3() else chr
+
+    _text_characters = (
+        b''.join(int2byte(i) for i in range(32, 127)) +
+        b'\n\r\t\f\b')
+
+    block = file_.read(blocksize)
+    if b'\x00' in block:
+        # Files with null bytes are binary
+        return False
+    elif not block:
+        # An empty file is considered a valid text file
+        return True
+
+    # Use translate's 'deletechars' argument to efficiently remove all
+    # occurrences of _text_characters from the block
+    non_text = block.translate(None, _text_characters)
+    return float(len(non_text)) / len(block) <= 0.30
 
 
 def get_random_bits(bits: int = 128) -> str:
