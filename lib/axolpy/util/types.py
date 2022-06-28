1b14651ebbc9902f673b4afa31325002f80ffb3a
tchiunam <tchiunam@users.noreply.github.com>
2022-06-28 23:27:06 +0800

    Add immutable dictionary

--- lib/axolpy/util/types.py
+++ lib/axolpy/util/types.py
@@ -17,3 +17,23 @@ class ThreadSafeDict(dict):
     # noinspection PyUnusedLocal
     def __exit__(self, type_, value, traceback) -> None:
         self._lock.release()
+
+
+class ImmutableDict(dict):
+    """
+    Immutable dictionary.
+    """
+
+    def __hash__(self):
+        return id(self)
+
+    def _immutable(self, *args, **kwargs):
+        raise TypeError('object is immutable')
+
+    __setitem__ = _immutable
+    __delitem__ = _immutable
+    clear = _immutable
+    update = _immutable
+    setdefault = _immutable
+    pop = _immutable
+    popitem = _immutable
