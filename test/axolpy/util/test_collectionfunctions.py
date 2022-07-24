a43313288bc2ce7fc1689b9a90068f3e0828ed94
tchiunam <tchiunam@users.noreply.github.com>
2022-07-24 22:49:08 +0800

    Test non-existing elements in test_expand_iter()

--- test/axolpy/util/test_collectionfunctions.py
+++ test/axolpy/util/test_collectionfunctions.py
@@ -7,6 +7,6 @@ def test_expand_iter() -> None:
     """
 
     assert list(cf.expand_iter(
-        elements=[2, 3],
+        elements=[2, 3, 6],
         from_elements=[1, 2, 3, 4, 5],
-        expand_func=lambda x: [x, x + 1])) == [2, 3, 3, 4]
+        expand_func=lambda x: [x, x + 1])) == [2, 3, 3, 4, 6]
