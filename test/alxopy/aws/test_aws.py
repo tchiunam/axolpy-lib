1b32058187fad14d0c5647c276a95612adf9ccf1
tchiunam <tchiunam@users.noreply.github.com>
2022-07-24 19:33:22 +0800

    Create EKS statefulset model

--- test/alxopy/aws/test_aws.py
+++ test/alxopy/aws/test_aws.py
@@ -1,7 +1,7 @@
-from axolpy.aws import EKSDeployment
+from axolpy.aws import EKSDeployment, EKSStatefulSet
 
 
-def test_eksdeployment():
+def test_eks_deployment():
     d = EKSDeployment(name="test", replicas=1, foo="bar")
     d.add_property(name="priority", value=2)
 
@@ -10,3 +10,14 @@ def test_eksdeployment():
     assert d.property("foo") == "bar"
     assert d.property("priority") == 2
     assert str(d) == "EKSDeployment(name: test, replicas: 1, 2 properties)"
+
+
+def test_eks_statefulset():
+    d = EKSStatefulSet(name="test", replicas=1, foo="bar")
+    d.add_property(name="priority", value=2)
+
+    assert d.name == "test"
+    assert d.replicas == 1
+    assert d.property("foo") == "bar"
+    assert d.property("priority") == 2
+    assert str(d) == "EKSStatefulSet(name: test, replicas: 1, 2 properties)"
