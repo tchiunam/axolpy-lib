52a4728c89e495d5ff2d09ff36b8e2bb11b03573
tchiunam <tchiunam@users.noreply.github.com>
2022-07-25 22:42:27 +0800

    Create AWS ECS Service model

--- src/axolpy/aws/__init__.py
+++ src/axolpy/aws/__init__.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Any, Dict
 
 from axolpy.kubernetes import Cluster
 
@@ -39,3 +39,35 @@ class AWSRegion(object):
 
     def __str__(self) -> str:
         return f"{__class__.__name__}(name: {self._name}, {len(self._eks_clusters)} EKS clusters)"
+
+
+class ECSService(object):
+    def __init__(self,
+                 name: str,
+                 desired_count: int = 0,
+                 **kwargs) -> None:
+        self._name: str = name
+        self._desired_count: int = desired_count
+        # Properties is used to store the properties of this ECS Service
+        # which are not the standard attributes of it.
+        self._properties: dict = dict()
+        for k, v in kwargs.items():
+            self.add_property(name=k, value=v)
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def desired_count(self) -> int:
+        return self._desired_count
+
+    def add_property(self, name: str, value: Any) -> None:
+        self._properties[name] = value
+
+    def property(self, name) -> Any:
+        return self._properties.get(name, None)
+
+    def __str__(self) -> str:
+        return f"{__class__.__name__}(name: {self._name}, desired_count: {self._desired_count}" + \
+            f", patch: {self._patch})"
