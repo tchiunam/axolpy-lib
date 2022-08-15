75cceb5a1c6027d2379de18104f6c5dd2ea5ec42
tchiunam <tchiunam@users.noreply.github.com>
2022-08-15 22:39:42 +0800

    Test RestartECSService._write_file_content() with the line of sleep

--- test/axolpy/cloudmaintenance/testdata/maintenance/dist-verify/operator1-1-update-ecs-task-count-RESUME.sh
+++ test/axolpy/cloudmaintenance/testdata/maintenance/dist-verify/operator1-1-update-ecs-task-count-RESUME.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 # aws ecs update-service --region ap-east-1 --cluster Production --service p-authentication-api --desired-count 10
+# sleep 2
+# aws ecs update-service --region ap-east-1 --cluster Production --service p-payproxy-api --desired-count 1
