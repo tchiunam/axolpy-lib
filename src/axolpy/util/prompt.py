caaf10ae7b258924a1eed3adc49e4ff709e338a3
tchiunam <tchiunam@users.noreply.github.com>
2022-08-20 23:03:44 +0800

    Create number validator for prompt input

--- src/axolpy/util/prompt.py
+++ src/axolpy/util/prompt.py
@@ -2,6 +2,36 @@ from prompt_toolkit.document import Document
 from prompt_toolkit.validation import ValidationError, Validator
 
 
+class NumberValidator(Validator):
+    """
+    A validator that validates if input is a number.
+    """
+
+    def validate(self, document: Document) -> None:
+        """
+        Validate if input is a number.
+
+        :param document: Input from the prompt.
+        :type document: :class:`prompt_toolkit.document.Document`
+
+        :raises: :class:`ValidationError` if input is not a number.
+        """
+
+        text = document.text
+
+        if text and not text.isdigit():
+            i = 0
+
+            # Get index of first non numeric character
+            # and move the cursor there.
+            for i, c in enumerate(text):
+                if not c.isdigit():
+                    break
+
+            raise ValidationError(message="Only number is allowed.",
+                                  cursor_position=i)
+
+
 class CryptographyKeyValidator(Validator):
     """
     A validator that validates if input is a cryptography key.
